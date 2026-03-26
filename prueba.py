4398ab20ba9997cb2649ad765f31357396dd39af
Josean <jamartinezfonseca@gmail.com>
2026-03-26 17:15:11 +0100

    commit fichero prueba

---
+++ prueba.py
@@ -0,0 +1,12 @@
+option = 0
+
+while option != "5":
+    print(f"1 - Crear tarea")
+    print(f"2 - Consultar tarea")
+    print(f"3 - Modificar tarea")
+    print(f"4 - Borrar tarea")
+    print(f"5 - Salir")
+
+    option = input("Introduce tarea: ")
+
+
